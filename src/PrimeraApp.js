9d84be1ec560faa0f597bd9f57ece2a36184e31d
oscardenas <oscardenas17@gmail.com>
2021-11-03 20:59:07 -0500

    Comunicación entre componentes- props

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 
-const PrimeraApp =  () => {
+const saludo = ' Hello World' ;
+
+const PrimeraApp =  ( {saludo = "hola, soy Goku¡"} ) => {
 
    return(
          <>
+            {/* { <pre> JSON.stringify(saludo, null, 3) }</pre> } */}
             <h1> Hola mundo</h1>
+            <p>{saludo}</p>
             <p>   START WITH REACT</p>
          </>
       );
