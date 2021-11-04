df6d3c6493a9add764120dae0e67c82907909918
oscardenas <oscardenas17@gmail.com>
2021-11-03 22:26:16 -0500

    Evento Click

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -2,10 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types'
 
 const CounterApp  = ( {contador} ) => {
+    
+    // handleAdd
+    const handleAdd = (e)=> {
+      console.log(e)
+    }
+    
+    
     return (
         <>
             <h1>Counter App</h1>
             <h2>{contador}</h2>
+
+            {/* <button onClick={ (e) => {handleAdd(e) } }> +1 </button> */}
+            <button onClick={ handleAdd }> +1 </button>
+            {/* funcion de fecla donde e es enviado como primer argumento al haddleadd s */}
         </>
 
     )
