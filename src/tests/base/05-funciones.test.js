6477014c2a307c2dc7f03bfa87e523a127facf72
oscardenas <oscardenas17@gmail.com>
2021-11-07 18:47:32 -0500

    Pruebas ToEqual, validar objetos

---
+++ src/tests/base/05-funciones.test.js
@@ -0,0 +1,38 @@
+import { getUser, getUsuarioActivo } from "../../base/05-funciones"
+
+
+
+describe('Pruebas en 05 funciones', () => {
+
+    test('getUser debe retornar un objeto ', () => {
+    
+    const userTest = {
+        uid: 'ABC123',
+        username: 'El_Papi1502'
+    }
+
+    const user = getUser();
+
+    expect (user).toEqual(userTest);
+
+    })
+
+    test('Get user retorna un objeto con nombre por parametro ', () => {
+        
+        const nombre = 'Yesid';
+        const userActivo ={
+            uid: 'ABC567',
+            username: nombre
+        }
+
+        const activo = getUsuarioActivo(nombre);
+
+        expect (activo).toEqual(userActivo)
+    })
+    
+
+
+   
+    
+})
+
